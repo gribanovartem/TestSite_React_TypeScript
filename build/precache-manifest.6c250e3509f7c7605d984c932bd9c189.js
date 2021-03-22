143bbdd8055a8d45d32c339c2c9f06115a2d57e7
gribanovartem <gribanovartem22@gmail.com>
2021-03-22 21:11:33 +0300

    add build folder

---
+++ build/precache-manifest.6c250e3509f7c7605d984c932bd9c189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4504b39bf4db9333c066e63bb1a9757e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ec99b20907d4121f2d9",
+    "url": "/static/css/main.f75ea567.chunk.css"
+  },
+  {
+    "revision": "bf3773a2492be4743ed6",
+    "url": "/static/js/2.511e3d90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.511e3d90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec99b20907d4121f2d9",
+    "url": "/static/js/main.982f575a.chunk.js"
+  },
+  {
+    "revision": "e6d860f2ba8cf386d80b",
+    "url": "/static/js/runtime-main.db9d0ab6.js"
+  }
+]);
